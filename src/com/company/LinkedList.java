58a70d754910864253c8b18c1ac3389d2fbab5d4
Mus'ap A <musapfc.tr@gmail.com>
2020-11-30 20:14:08 +0300

    JavaCodes

---
+++ src/com/company/LinkedList.java
@@ -0,0 +1,64 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class LinkedList {
+
+    public static class Node{
+        Object value;
+        Node next;
+        public Node(Object value, Node next){
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    Node headNode;
+
+    public LinkedList(){
+        headNode = null;
+    }
+
+    public void add(Object val){
+        Node newNode = new Node(val,null);
+        if (headNode != null) {
+            newNode.next = headNode;
+        }
+        headNode = newNode;
+    }
+
+    public void delete(){
+        headNode = headNode.next;
+    }
+
+    public void display(){
+        Node n = headNode;
+        while(n != null){
+            System.out.println(n.value);
+            n = n.next;
+        }
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        for (String s : Arrays.asList("Aşkın", "Musap", "Computer Science")) {
+            linkedList.add(s);
+        }
+        linkedList.display();
+        System.out.println("-----------");
+        linkedList.delete();
+        System.out.println("Deleting...");
+        System.out.println("-----------");
+        linkedList.display();
+        System.out.println("-----------");
+        linkedList.delete();
+        System.out.println("Deleting...");
+        System.out.println("-----------");
+        linkedList.display();
+
+
+
+    }
+}
