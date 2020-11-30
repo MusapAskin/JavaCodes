3863a1ca062139e570f521f34594793775d9e060
Mus'ap A <musapfc.tr@gmail.com>
2020-12-01 01:51:01 +0300

    JavaCodes

---
+++ src/com/company/DoubleLinkedList.java
@@ -0,0 +1,69 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class DoubleLinkedList {
+
+    Node headNode;
+
+    public static class Node{
+        Object value;
+        Node next;
+        Node previous;
+        public Node(Object value, Node previous, Node next){
+            this.value = value;
+            this.next = next;
+            this.previous = previous;
+        }
+    }
+
+    public DoubleLinkedList(){
+        headNode = null;
+    }
+
+    public void add(Object val){
+        Node newNode = new Node(val,null,null);
+        if (headNode == null) {
+             headNode = newNode;
+        }else {
+            newNode.next = headNode;
+            headNode.previous = newNode;
+            headNode = newNode;
+        }
+    }
+
+    public void delete(){
+        headNode = headNode.next;
+        headNode.previous = null;
+    }
+
+    public void display(){
+        Node n = headNode;
+        while(n != null){
+            System.out.println(n.value);
+            n = n.next;
+        }
+    }
+}
+
+class Double {
+    public static void main(String[] args) {
+        DoubleLinkedList linkedList = new DoubleLinkedList();
+        for (String s : Arrays.asList("Aşkın", "Musap", "Computer scientist")) {
+                linkedList.add(s);
+        }
+        linkedList.display();
+        System.out.println("-----------");
+        linkedList.delete();
+        System.out.println("Deleting...");
+        System.out.println("-----------");
+        linkedList.display();
+        System.out.println("-----------");
+        linkedList.delete();
+        System.out.println("Deleting...");
+        System.out.println("-----------");
+        linkedList.display();
+    }
+}
+
+
